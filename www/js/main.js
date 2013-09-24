494af2b4ba11763bc983477d5a7a71480494eecd
lpiguet <laurent.piguet@gmail.com>
2013-09-24 06:24:54 -0700

    - Make space on page

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,8 @@
 function update_div (in_id, in_url) {
-    alert ('Getting url:'+in_url);
+    //    alert ('Getting url:'+in_url);
+    $('#'+in_id).html('<img src="img/loading.gif" alt="" />');
+    $('#'+in_id).show();
+
     $.ajax({
             type: "GET",
                 url: in_url,
