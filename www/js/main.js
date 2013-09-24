37818920821518dd9234fd7a2b277172b52d1e5a
lpiguet <laurent.piguet@gmail.com>
2013-09-24 05:52:41 -0700

    - Debug stuff

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,10 @@
 function update_div (in_id, in_url) {
-
+    alert ('Getting url:'+in_url);
     $.ajax({
             type: "GET",
                 url: in_url,
                 success: function (html) {$('#'+in_id).html(html);},
+                error: function (jqXHR, textStatus, errorThrown) {alert (textStatus)},
            });
 
 }
