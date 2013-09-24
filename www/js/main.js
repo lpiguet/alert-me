c1ca2040a9748b415aa967a122adadcb032d40b2
lpiguet <laurent.piguet@gmail.com>
2013-09-24 03:37:33 -0700

    - Updates

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,9 @@
+function update_div (in_id, in_url) {
 
+    $.ajax({
+            type: "GET",
+                url: in_url,
+                success: function (html) {$('#'+in_id).html(html);},
+           });
+
+}
