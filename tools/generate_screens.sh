120d614ebba0f215aad61994daa3f74b8850f5af
lpiguet <laurent.piguet@gmail.com>
2013-09-25 01:55:00 -0700

    - Updated splash

--- tools/generate_screens.sh
+++ tools/generate_screens.sh
@@ -9,7 +9,7 @@ PLAT=android
 for SIZE in 800x480 480x800 320x200 200x320 480x320 320x480 1280x720 720x1280
 do
 echo Generating $PLAT $SIZE
-convert $SRC -background "#d6d6ee" -gravity center -extent $SIZE -alpha on -flatten $OUT/$PLAT/screen-$SIZE.png
+convert $SRC -background "#000000" -gravity center -extent $SIZE -alpha on -flatten $OUT/$PLAT/screen-$SIZE.png
 done
 
 # iOS
@@ -17,5 +17,5 @@ PLAT=ios
 for SIZE in 2008x1536 1024x783 1536x2008 768x1004 960x640 480x320 640x960 640x1136 320x480
 do
 echo Generating $PLAT $SIZE
-convert $SRC -background "#d6d6ee" -gravity center -extent $SIZE -alpha on -flatten $OUT/$PLAT/screen-$SIZE.png
+convert $SRC -background "#000000" -gravity center -extent $SIZE -alpha on -flatten $OUT/$PLAT/screen-$SIZE.png
 done
