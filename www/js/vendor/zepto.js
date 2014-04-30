4aa2d81fe28fba0d76d5064b310736320bdadd87
lpiguet <laurent.piguet@gmail.com>
2014-04-30 16:34:04 +0200

    - Test

--- www/js/vendor/zepto.js
+++ www/js/vendor/zepto.js
@@ -1323,10 +1323,7 @@ window.$ === undefined && (window.$ = Zepto)
 
     ajaxStart(settings)
 
-      if (!settings.crossDomain) {
-          settings.crossDomain = /^([\w-]+:)?\/\/([^\/]+)/.test(settings.url) && RegExp.$2 != window.location.host
-          console.log ('settings.crossDomain: '+window.location.host+ ' : '+settings.crossDomain);
-      }
+    if (!settings.crossDomain && window.location.host !== undefined) settings.crossDomain = /^([\w-]+:)?\/\/([^\/]+)/.test(settings.url) && RegExp.$2 != window.location.host
 
     if (!settings.url) settings.url = window.location.toString()
     serializeData(settings)
@@ -1350,9 +1347,7 @@ window.$ === undefined && (window.$ = Zepto)
 
     if (deferred) deferred.promise(xhr)
 
-      if (!settings.crossDomain) {
-          setHeader('X-Requested-With', 'XMLHttpRequest')
-      }
+    if (!settings.crossDomain) setHeader('X-Requested-With', 'XMLHttpRequest')
     setHeader('Accept', mime || '*/*')
     if (mime = settings.mimeType || mime) {
       if (mime.indexOf(',') > -1) mime = mime.split(',', 2)[0]
